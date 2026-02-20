177dc031d2b296fcc387787a69334d34ef62c76b
pratyusha <pratyusha.gh@gmail.com>
2026-02-20 13:57:08 +0530

    chore: move into subfolder

--- src/hotel_price_visualisation/config.py
+++ src/hotel_price_visualisation/config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parents[1]
+BASE_DIR = Path(__file__).resolve().parent
 ASSETS_DIR = BASE_DIR / "assets"
 DATA_FILE = ASSETS_DIR / "multi_year_price_data.csv"
